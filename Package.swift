c27230bc786b26597a269a0e6aa2a3cd2eac82c9
Guboneui <starku2249@naver.com>
2023-11-17 01:12:09 +0900

    feat: Add Package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NMapsMap",
+    defaultLocalization: "ko",
+    platforms: [
+      .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "NMapsMap",
+            targets: ["NMapsMap"]),
+    ],
+    targets: [
+        .binaryTarget(
+          name: "NMapsMap",
+          url: "https://github.com/navermaps/NMapsMap/releases/download/release%2F3.17.0/NMapsMap.framework.zip",
+          checksum: "1ad233a0bd2a5ffb4a83e34279be73b721e3f2940f82bccaf86619b3d368da27"
+        )
+    ]
+)
