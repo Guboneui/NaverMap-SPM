ac936b0883e9d43887afaaf9f37eaf330e5a923f
보니 <73548875+Guboneui@users.noreply.github.com>
2023-11-17 02:09:48 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,11 @@ let package = Package(
           name: "NMapsMap",
           url: "https://github.com/navermaps/NMapsMap/releases/download/release%2F3.17.0/NMapsMap.framework.zip",
           checksum: "1ad233a0bd2a5ffb4a83e34279be73b721e3f2940f82bccaf86619b3d368da27"
+        ),
+        .binaryTarget(
+          name: "NMapsGeometry",
+          url: "https://github.com/Guboneui/NMapsGeometry-Framework/releases/download/v1.0.1/NMapsGeometry.framework.zip",
+          checksum: "41cb10080f2f152711c796a22132d3f5b6d124fc1d28d16dd6e3f59beed78d63"
         )
     ]
 )
